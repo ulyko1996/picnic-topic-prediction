e3afd07e3c81eb68a506e34694861361aa93cc7c
Ulysses <61469350+ulyko1996@users.noreply.github.com>
2025-10-20 19:23:15 +0100

    Add lemmatizer

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.13-slim-trixie
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
+WORKDIR /app
+
 # Download libgomp that is essential for LightGBM
 RUN apt-get update && apt-get install -y --no-install-recommends apt-utils
 RUN apt-get -y install curl
@@ -16,5 +18,8 @@ RUN --mount=type=cache,target=/root/.cache/uv \
 COPY . /app
 RUN --mount=type=cache,target=/root/.cache/uv \
     uv sync --locked --no-dev
+
+# Copy wordnet corpus to NLTK corpus folder
+COPY resource/wordnet /root/nltk_data/corpora/wordnet
 
 CMD uv run main.py
--- picnic_topic_prediction/train.py
+++ picnic_topic_prediction/train.py
@@ -1,17 +1,25 @@
-from typing import Any, Dict
 from picnic_topic_prediction.utils import load_training_data
 from picnic_topic_prediction.config import OptunaSearchCVConfig, PARAMETER_GRID
 
 from optuna_integration import OptunaSearchCV
 from sklearn.pipeline import Pipeline
 
+import re
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction._stop_words import ENGLISH_STOP_WORDS
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from lightgbm import LGBMClassifier
 
 import mlflow
 
+# The lemmatizer can be passed to the tokenizer argument of TfidfVectorizer
+LEMMATIZER = lambda text: [WordNetLemmatizer().lemmatize(token) 
+                           for token in re.compile('(?u)\\b\\w\\w+\\b').findall(text) 
+                           if token not in ENGLISH_STOP_WORDS]
+
 MODELS = {
-    'tfidf_lgb': Pipeline([('tfidf', TfidfVectorizer(stop_words='english')), 
+    'tfidf_lgb': Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
                            ('lightgbm', LGBMClassifier(objective='multiclass', random_state=42, verbose=-1))])
     }
 
