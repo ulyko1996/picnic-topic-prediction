44905f3fc60feb5f508fc6d1a45bc1a8c5aefd00
Ulysses <61469350+ulyko1996@users.noreply.github.com>
2025-10-20 09:54:42 +0100

    Add error raising for get_param_grid

--- picnic_topic_prediction/utils.py
+++ picnic_topic_prediction/utils.py
@@ -14,5 +14,9 @@ def load_test_data():
 def get_param_grid(model_type: str):
     with open(get_root_directory() + '/picnic_topic_prediction/param_grid.yaml', 'r') as file:
         param_grid = yaml.safe_load(file).get(model_type, None)
-        
+    
+    # Raise error if parameter grid config is not found for the model type 
+    if param_grid is None:
+        raise ValueError(f'Model type {model_type} not found')
+
     return param_grid
