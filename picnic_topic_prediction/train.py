d5da6b623f3774e81a69ef4c22ba7f1cbbb5f1be
Ulysses <61469350+ulyko1996@users.noreply.github.com>
2025-10-20 17:01:31 +0100

    Move hp grid to config

--- picnic_topic_prediction/train.py
+++ picnic_topic_prediction/train.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 from picnic_topic_prediction.utils import load_training_data
-from picnic_topic_prediction.config import OptunaSearchCVConfig
-from picnic_topic_prediction.hp_tuning import get_param_grid
+from picnic_topic_prediction.config import OptunaSearchCVConfig, PARAMETER_GRID
 
 from optuna_integration import OptunaSearchCV
 from sklearn.pipeline import Pipeline
@@ -12,16 +11,17 @@ from lightgbm import LGBMClassifier
 import mlflow
 
 MODELS = {
-    'tfidf_lgb': Pipeline([('tfidf_vectorizer', TfidfVectorizer(stop_words='english')), 
+    'tfidf_lgb': Pipeline([('tfidf', TfidfVectorizer(stop_words='english')), 
                            ('lightgbm', LGBMClassifier(objective='multiclass', random_state=42, verbose=-1))])
     }
 
 def train_model(model_type:str = 'tfidf_lgb'):
+    training_data = load_training_data()
     output = OptunaSearchCV(estimator=MODELS.get(model_type), 
-                            param_distributions=get_param_grid(model_type), 
+                            param_distributions=PARAMETER_GRID.get(model_type), 
                             n_jobs=-1,
                             **OptunaSearchCVConfig().model_dump(),
-                            ).fit(load_training_data()['text'], load_training_data()['label'])
+                            ).fit(training_data['text'], training_data['label'])
     
     mlflow.log_params(output.best_params_)
     mlflow.log_metric('Training accuracy', output.best_score_)
