9711d8651aa5f36855271ec75ed5a367fe9961eb
Ulysses <61469350+ulyko1996@users.noreply.github.com>
2025-10-20 17:03:03 +0100

    Add MLflow and Docker (#4)
    * Switch to docker
    
    * Change verbosity of LightGBM
    
    * Move models out of function
    
    * Add basic mlflow logging
    
    * Move hp grid to config

--- picnic_topic_prediction/config.py
+++ picnic_topic_prediction/config.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel
+from optuna.distributions import IntDistribution, FloatDistribution, CategoricalDistribution
 
 class OptunaSearchCVConfig(BaseModel):
     cv: int = 5
     scoring: str = 'accuracy'
     n_trials: int = 1
+    
+PARAMETER_GRID = {
+    'tfidf_lgb': {
+        'tfidf__ngram_range': CategoricalDistribution([(1,1), (1,2)]),
+        'lightgbm__n_estimators': IntDistribution(low=10, high=200, step=10),
+        'lightgbm__num_leaves': IntDistribution(low=4, high=64, step=4),
+        'lightgbm__max_depth': CategoricalDistribution([-1, 4, 8, 16, 32]),
+        'lightgbm__learning_rate': FloatDistribution(low=0.01, high=0.1, step=0.01),
+        'lightgbm__subsample': FloatDistribution(low=0.8, high=1, step=0.05),
+        'lightgbm__subsample_freq': IntDistribution(low=0, high=1, step=1),
+        'lightgbm__colsample_bytree': FloatDistribution(low=0.8, high=1, step=0.05),
+    }
+}
--- picnic_topic_prediction/train.py
+++ picnic_topic_prediction/train.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 from picnic_topic_prediction.utils import load_training_data
-from picnic_topic_prediction.config import OptunaSearchCVConfig
-from picnic_topic_prediction.hp_tuning import get_param_grid
+from picnic_topic_prediction.config import OptunaSearchCVConfig, PARAMETER_GRID
 
 from optuna_integration import OptunaSearchCV
 from sklearn.pipeline import Pipeline
@@ -9,16 +8,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from lightgbm import LGBMClassifier
 
+import mlflow
+
+MODELS = {
+    'tfidf_lgb': Pipeline([('tfidf', TfidfVectorizer(stop_words='english')), 
+                           ('lightgbm', LGBMClassifier(objective='multiclass', random_state=42, verbose=-1))])
+    }
+
 def train_model(model_type:str = 'tfidf_lgb'):
-    models = {'tfidf_lgb': Pipeline([('tfidf_vectorizer', TfidfVectorizer(stop_words='english')), 
-                                     ('lightgbm', LGBMClassifier(objective='multiclass', random_state=42, verbosity=0))])}
-    
-    output = OptunaSearchCV(estimator=models.get(model_type), 
-                            param_distributions=get_param_grid(model_type), 
+    training_data = load_training_data()
+    output = OptunaSearchCV(estimator=MODELS.get(model_type), 
+                            param_distributions=PARAMETER_GRID.get(model_type), 
                             n_jobs=-1,
                             **OptunaSearchCVConfig().model_dump(),
-                            ).fit(load_training_data()['text'], load_training_data()['label'])
+                            ).fit(training_data['text'], training_data['label'])
     
-    print(output.cv_results_)
-    print(output.best_score_)
+    mlflow.log_params(output.best_params_)
+    mlflow.log_metric('Training accuracy', output.best_score_)
     return output.best_estimator_
