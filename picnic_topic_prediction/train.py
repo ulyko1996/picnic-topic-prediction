22b84cfb44fb59411788329b8d1231679bb420d7
Ulysses <61469350+ulyko1996@users.noreply.github.com>
2025-10-21 01:06:26 +0100

    Add feature importance graph

--- picnic_topic_prediction/train.py
+++ picnic_topic_prediction/train.py
@@ -10,8 +10,10 @@ import re
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction._stop_words import ENGLISH_STOP_WORDS
 from sklearn.feature_extraction.text import TfidfVectorizer
+import lightgbm as lgb
 from lightgbm import LGBMClassifier
 
+import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
 
 # The lemmatizer can be passed to the tokenizer argument of TfidfVectorizer
@@ -42,4 +44,13 @@ def train_model(model_type:str = 'tfidf_lgb'):
         'Training F1 Score': f1_score(y_train, y_pred, average='macro'),
     })
     mlflow.log_figure(create_confusion_matrix(y_train, y_pred), "Training Confusion Matrix.png")
+    
+    if model_type == 'tfidf_lgb':
+        lookup_dict = {value: key for key, value in output.best_estimator_['tfidf'].vocabulary_.items()}
+        feature_importance_fig, ax = plt.subplots(figsize=(10, 6))
+        ax = lgb.plot_importance(output.best_estimator_['lightgbm'], ax=ax, importance_type="gain", title="LightGBM Feature Importance (Gain)", max_num_features=20)
+        ax.set_yticklabels([lookup_dict.get(int(label.get_text().split('_')[-1])) for label in ax.get_yticklabels()])
+        mlflow.log_figure(feature_importance_fig, "LightGBM Feature Importance (Gain).png")
+        plt.close(feature_importance_fig)
+    
     return output.best_estimator_
